b05a1abcc9383618c45e529c8a7444509f72bcc6
lolli-17 <lollomannoni@hotmail.com>
2025-12-14 02:22:45 +0100

    Quadrato che si muove

--- main.lua
+++ main.lua
@@ -1,11 +1,25 @@
 function love.load()
-    
+    player = {
+        x = 400 - 15,
+        y = 300 - 15,
+        size = 30,
+        speed = 200,
+    }
 end
 
-function love.update()
-    
+function love.update(dt)
+    if love.keyboard.isDown("right") then
+        player.x = player.x + (player.speed * dt)
+    elseif love.keyboard.isDown("left") then
+        player.x = player.x - (player.speed * dt)
+    end
+    if love.keyboard.isDown("down") then
+        player.y = player.y + (player.speed * dt)
+    elseif love.keyboard.isDown("up") then
+        player.y = player.y - (player.speed * dt)
+    end
 end
 
 function love.draw()
-    love.graphics.print("Tutto funziona!", 350, 200)
+    love.graphics.rectangle("fill", player.x, player.y, player.size, player.size)
 end
